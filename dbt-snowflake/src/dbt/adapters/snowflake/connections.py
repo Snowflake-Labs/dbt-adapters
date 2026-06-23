f4bd851787abcf2dec0f813c177d71097775b27c
sfc-gh-zgong <zibo.gong@snowflake.com>
2026-06-23 16:48:18 -0700

    ignore missing account

--- dbt-snowflake/src/dbt/adapters/snowflake/connections.py
+++ dbt-snowflake/src/dbt/adapters/snowflake/connections.py
@@ -177,12 +177,12 @@ class SnowflakeCredentials(Credentials):
                 warn_or_error(
                     AdapterEventError(base_msg="Invalid profile: 'user' is a required property.")
                 )
-
-        self.account, sub_count = re.subn("_", "-", self.account)
-        if sub_count:
-            logger.debug(
-                "Replaced underscores (_) with hyphens (-) in Snowflake account name to form a valid account URL."
-            )
+        if self.account:
+            self.account, sub_count = re.subn("_", "-", self.account)
+            if sub_count:
+                logger.debug(
+                    "Replaced underscores (_) with hyphens (-) in Snowflake account name to form a valid account URL."
+                )
 
         # only default `reuse_connections` to `True` if the user has not turned on `client_session_keep_alive`
         # having both of these set to `True` could lead to hanging open connections, so it should be opt-in behavior
